68684f6ee3dd46a73372d73e84a1d847a0db389c
11027649 <natasjawezel@hotmail.com>
2018-09-10 15:52:35 +0200

    Opzetje voor algoritme

--- EvolutionaryAlgorithm.java
+++ EvolutionaryAlgorithm.java
@@ -1,7 +1,27 @@
+/*
+ * This is an evolutionary algorithm to optimize three continuous optimization
+ * problems: Bent Cigar, Katsuura and Schaffers F7 functions.
+ *
+ * Bob, Emma, Tobias, Natasja
+ * Evolutionary Computing Course 2018
+ *
+ */
+
 public class EvolutionaryAlgorithm {
   public static void main(String[] args) {
 
+    // TODO: Create an initial population
     System.out.println("Initializing population....");
 
+    // Until "done"
+    // Select pairs to be parents
+
+    // Recombination
+
+    // Mutations
+
+    // Select survivors
+
+    // Repeat until termination criterion is reached
   }
 }
