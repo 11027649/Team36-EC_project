1b65afe5035f7a28c6400752bbeedb1242cd675c
tobiasxg <tobias.garritsen@hotmail.com>
2018-09-17 12:39:44 +0200

    Gorilla baas bangt alle chicks

--- player36.java
+++ player36.java
@@ -53,9 +53,11 @@ public class player36 implements ContestSubmission
 
 		int evals = 0;
 		double childrens[][] = create_population();
-
+		int glb_best = 0;
+		
 		// calculate fitness
 		while (evals < evaluations_limit_) {
+		// for (int generations = 0; generations < 100; generations++) {
 
 			// Select parents
 
@@ -63,12 +65,36 @@ public class player36 implements ContestSubmission
 
 			// Check fitness of unknown fuction: determines your grade
 			// needs real-value values but conversions before are okay
-			Double fitness = (double) evaluation_.evaluate(childrens[2]);
-			evals++;
+			// Double fitness = (double) evaluation_.evaluate(childrens[2]);
 
-			// Select survivors
+			int best = 0;
+			double best_value = -100;
+			for (int i = 0; i < childrens.length; i++) {
+				// System.out.println(Arrays.toString(childrens[i]));
+				// System.out.println(i);
+				Double fitness = (double) evaluation_.evaluate(childrens[i]);
+				// System.out.println(fitness);
+				if (fitness > best_value) {
+					best_value = fitness;
+					best = i;
+				}
+				evals++;
+			}
+
+			// System.out.println(best_value);
+			System.out.println(best);
+			// System.out.println(Arrays.toString(childrens[best]));
+			for (int j = 0; j < childrens.length; j++) {
+				for (int c = 0; c < childrens[j].length; c++) {
+					childrens[j][c] = (childrens[j][c]+childrens[best][c])/2;
+				}
+			}
+
+			glb_best = best;
 
 		}
+
+		System.out.println(Arrays.toString(childrens[glb_best]));
 	}
 
 
@@ -80,7 +106,7 @@ public class player36 implements ContestSubmission
 		double children[][] = new double[pop_size][];
 
 		// initialize population randomly
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < pop_size; i++) {
 			double child[] = new double[dim];
 
 			for (int j = 0; j < dim; j++) {
@@ -91,7 +117,10 @@ public class player36 implements ContestSubmission
 
 			}
 			children[i] = child;
+			// System.out.println(Arrays.toString(children[i]));
 		}
+		double[] curr_top = new double[] {-1.1891876987039534, 3.802704764222131, -0.7045031488811008, -3.214820204008321, 0.9342012108821499, -1.6280075915460186, 1.1164003795515511, -0.7854278491364934, 1.7478175537980336, -0.45014115827163426};
+		children[1] = curr_top;
 		return children;
 	}
 }
